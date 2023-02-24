5b1bf752c1203f50399be41a717327fb2fd63fe5
Marcha <marchaf@gmail.com>
2023-02-24 14:47:36 +0100

    init recoil add atom on authModal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
-import { theme } from '../chakra/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { theme } from "../chakra/theme";
+import Layout from "../components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
+  );
 }
-
-
-
-
