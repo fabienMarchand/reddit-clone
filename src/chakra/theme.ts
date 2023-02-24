d3c658739ba466ab952c65f73a3cfc243cde5978
Marcha <marchaf@gmail.com>
2023-02-24 14:47:07 +0100

    add theme for chakra ui, add button images and styles

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,8 +1,9 @@
 // 1. Import `extendTheme`
 import { extendTheme } from "@chakra-ui/react";
-import "@fontsource/open-sans/300.css";
-import "@fontsource/open-sans/400.css";
-import "@fontsource/open-sans/700.css";
+import { Button } from "./button";
+// import "@fontsource/open-sans/300.css";
+// import "@fontsource/open-sans/400.css";
+// import "@fontsource/open-sans/700.css";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
@@ -21,7 +22,7 @@ export const theme = extendTheme({
       },
     }),
   },
-  components :{
-    // Button
-  }
+  components: {
+    Button,
+  },
 });
