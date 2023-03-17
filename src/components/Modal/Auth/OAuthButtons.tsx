b57e485f8060b357c4b188858294511e0d5eb47e
Marcha <marchaf@gmail.com>
2023-03-17 14:16:46 +0100

    add login and signUp ui

---
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -0,0 +1,20 @@
+import { Button, Flex, Image } from "@chakra-ui/react";
+import React from "react";
+
+// type OAuthButtonsProps = {
+
+// };
+
+// const OAuthButtons:React.FC<OAuthButtonsProps> = () => {
+const OAuthButtons: React.FC = () => {
+  return (
+    <Flex direction="column" width="100%" mb={4}>
+      <Button variant="oauth" mb={2}>
+        <Image src="/images/googlelogo.png" height="20px" mr={4} />
+        Continue with Google
+      </Button>
+      <Button variant="oauth">Some Other Provider</Button>
+    </Flex>
+  );
+};
+export default OAuthButtons;
