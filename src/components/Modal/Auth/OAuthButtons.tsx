cc58160300ccb18fb5f0ad63b19492a43eed9358
Marcha <marchaf@gmail.com>
2023-09-28 16:24:11 +0200

    add oauth features with communities

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Flex, Image, Text } from "@chakra-ui/react";
 
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { auth } from "../../../firebase/clientApp";
+import { auth, firestore } from "../../../firebase/clientApp";
+import { doc, setDoc } from "firebase/firestore";
+import { User } from "firebase/auth";
 
 // type OAuthButtonsProps = {};
 
 // const OAuthButtons:React.FC<OAuthButtonsProps> = () => {
 const OAuthButtons: React.FC = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, userCred, loading, error] =
+    useSignInWithGoogle(auth);
+
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, "users", user.uid);
+    await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+  };
+
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
 
   return (
     <Flex direction="column" width="100%" mb={4}>
